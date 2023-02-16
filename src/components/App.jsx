f3885148ca522bef96f972c7ed2c4e1c6ca92538
nnatroo <nugonatroshvili@gmail.com>
2023-02-16 21:11:25 +0400

    added local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
 
-  const [notes, setNotes] = useState([])
+  // useEffect(() => {
+  //   let localNotes = localStorage.getItem("notes");
+  //   if (localNotes) {
+  //     localNotes = JSON.parse(localStorage.getItem("notes"));
+  //   } else {
+  //     localNotes = [];
+  //   }
+  //   setNotes(localNotes);
+  //   // console.log(notes);
+  // }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem("notes", JSON.stringify(notes));
+  // }, [notes]);
 
   const handleAdd = (newNote) => {
     setNotes((prevNotes) => {
-      return [...prevNotes, newNote]
-    })
-  }
+      return [...prevNotes, newNote];
+    });
+  };
 
   const handleDelete = (id) => {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return prevNotes.filter((note, index) => {
-        return index !== id
-      }
-    )})
-
-  }
+        return index !== id;
+      });
+    });
+  };
 
   return (
     <div>
       <Header />
-      <CreateArea onAdd={handleAdd}/>
-
+      <CreateArea onAdd={handleAdd} />
       {notes.map((note, index) => (
-        <Note onDelete={handleDelete} id={index} key={index} title={note.title} content={note.content} />
+        <Note
+          onDelete={handleDelete}
+          id={index}
+          key={index}
+          title={note.title}
+          content={note.content}
+        />
       ))}
 
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,64 +1,70 @@
 import React, { useState } from "react";
-import AddIcon from '@mui/icons-material/Add';
+import AddIcon from "@mui/icons-material/Add";
 import { Fab } from "@mui/material";
-import { Zoom } from '@mui/material';
-
+import { Zoom } from "@mui/material";
 
 function CreateArea(props) {
+  const [note, setNote] = useState({ title: "", content: "" });
 
-  const [note, setNote] = useState({title: "", 
-                                    content: ""});
-  
-  const [collapseArea, setCollapseArea] = useState(true)
+  const [collapseArea, setCollapseArea] = useState(true);
 
   const handleChange = (event) => {
-    const inputName = event.target.name
-    const inputValue = event.target.value
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
 
     setNote((prevValue) => {
       return {
         ...prevValue,
         [inputName]: inputValue,
-      }
-    })
-  }
-
+      };
+    });
+  };
 
   const handleSubmit = (event) => {
-    if (note.title !== "" || note.content !== ""){
+    if (note.title !== "" || note.content !== "") {
       props.onAdd(note);
       setNote({
         title: "",
-        content: ""
-    })
+        content: "",
+      });
     } else {
-      alert("Fill each of these input !")
+      alert("Fill at least one input!");
     }
 
     event.preventDefault();
-    
-  }
+  };
 
   const handleFocus = () => {
     setCollapseArea(false);
-  }
-  
+  };
+
   return (
     <div>
       <form className="create-note" onSubmit={handleSubmit}>
-        {!collapseArea?<input value={note.title} onChange={handleChange} name="title" placeholder="Title" />: null}
-        <textarea onFocus={handleFocus} value={note.content} onChange={handleChange} name="content" placeholder="Take a note..." rows={collapseArea? 1 : 3} />
+        {!collapseArea ? (
+          <input
+            value={note.title}
+            onChange={handleChange}
+            name="title"
+            placeholder="Title"
+          />
+        ) : null}
+        <textarea
+          onFocus={handleFocus}
+          value={note.content}
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows={collapseArea ? 1 : 3}
+        />
         <Zoom in={!collapseArea}>
           <Fab type="submit" color="primary" aria-label="add">
             <AddIcon />
           </Fab>
         </Zoom>
-        
       </form>
     </div>
   );
 }
-
-
 
 export default CreateArea;
